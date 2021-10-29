b55c940f0a5ab3942188f113e23536f91c6e22c3
Marcin Jarosz <marcin.jarosz@quarticon.com>
2021-10-29 19:01:35 +0200

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {WarningAlertComponent} from "./components/warning-alert/warning-alert.component";
+import {SuccessAlertComponent} from "./components/success-alert/success-alert.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
