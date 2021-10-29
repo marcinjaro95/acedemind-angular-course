e00df423c679a4c2476bb27a45032acec2015795
Marcin Jarosz <marcin.jarosz@quarticon.com>
2021-10-29 19:29:31 +0200

    databinding commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {WarningAlertComponent} from "./components/warning-alert/warning-alert.component";
-import {SuccessAlertComponent} from "./components/success-alert/success-alert.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
