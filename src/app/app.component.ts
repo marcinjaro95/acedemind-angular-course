5387692fd788dc27f245297d39dc4ca168b10397
Marcin Jarosz <marcin.jarosz@quarticon.com>
2021-11-04 14:56:06 +0100

    tdf commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {UserService} from "./services/user.service";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [UserService]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  activeUsers: string[] = [];
-  inactiveUsers: string[] = [];
-
-  constructor(private usersService: UserService) {
-  }
-
-  ngOnInit() {
-    this.activeUsers = this.usersService.activeUsers;
-    this.inactiveUsers = this.usersService.inactiveUsers;
-  }
+export class AppComponent {
 
 }
---
+++ src/app/components/models/form-data.ts
@@ -0,0 +1,5 @@
+export interface FormData {
+  email: string,
+  subscription: string,
+  password: string
+}
